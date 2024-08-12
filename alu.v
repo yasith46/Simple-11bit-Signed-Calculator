c289cb916ac90b5feda8688003145d9c668be25e
yasith46 <yasithudana3@gmail.com>
2024-08-12 22:39:26 +0530

    Updated alu.v, PC.qsf

--- alu.v
+++ alu.v
@@ -1,24 +1,28 @@
 module alu(
-		input [31:0] A,
-		input [31:0] B,
+		input [15:0] A,
+		input [15:0] B,
 		input [ 2:0] CTRL,
 	
-		output [31:0] RESULT,
+		output reg [15:0] RESULT,
 		output ZEROFLAG, NEGATIVEFLAG
 	);
 	
 	
-	wire CIN,COUT0,COUT1,COUT2,COUT3,COUT4,COUT5,COUT6,COUT7;
-	wire [31:0] B_INT;
+	wire [15:0] RESULT_AND;
+	wire [15:0] RESULT_OR;
+	wire [15:0] RESULT_SUMSUB;
+	
+	// Logic of AND and OR
+	assign RESULT_OR = A | B;
 	
 	
 	// Logic for subtraction 
-	assign CIN = 
-		(CTRL == 3'b000) ? 1'b0 :
-		(CTRL == 3'b001) ? 1'b1 :
-		1'bx;
+	reg CIN;
+	wire COUT0,COUT1,COUT2,COUT3;
+	wire [15:0] B_INT;
 		
 	assign B_INT = B ^ {8{CIN}};
+	
 	
 	
 	// Adders and subtractors	
@@ -27,7 +31,8 @@ module alu(
 		.B( B_INT[3:0] ),
 		.CIN( CIN ),
 		.COUT( COUT0 ),
-		.SUM( RESULT[3:0] )
+		.SUM( RESULT_SUMSUB[3:0] ),
+		.AND_OUT( RESULT_AND[3:0] )
 	);
 	
 	cla cla1 (
@@ -35,7 +40,8 @@ module alu(
 		.B( B_INT[7:4] ),
 		.CIN( COUT0 ),
 		.COUT( COUT1 ),
-		.SUM( RESULT[7:4] )
+		.SUM( RESULT_SUMSUB[7:4] ),
+		.AND_OUT( RESULT_AND[7:4] )
 	);
 	
 	cla cla2 (
@@ -43,7 +49,8 @@ module alu(
 		.B( B_INT[11:8] ),
 		.CIN( COUT1 ),
 		.COUT( COUT2 ),
-		.SUM( RESULT[11:8] )
+		.SUM( RESULT_SUMSUB[11:8] ),
+		.AND_OUT( RESULT_AND[11:8] )
 	);
 	
 	cla cla3 (
@@ -51,39 +58,53 @@ module alu(
 		.B( B_INT[15:12] ),
 		.CIN( COUT2 ),
 		.COUT( COUT3 ),
-		.SUM( RESULT[15:12] )
+		.SUM( RESULT_SUMSUB[15:12] ),
+		.AND_OUT( RESULT_AND[15:12] )
 	);
 	
-	cla cla4 (
-		.A( A[19:16] ),
-		.B( B_INT[19:16] ),
-		.CIN( COUT3 ),
-		.COUT( COUT4 ),
-		.SUM( RESULT[19:16] )
-	);
-	
-	cla cla5 (
-		.A( A[23:20] ),
-		.B( B_INT[23:20] ),
-		.CIN( COUT4 ),
-		.COUT( COUT5 ),
-		.SUM( RESULT[23:20] )
-	);
-	
-	cla cla6 (
-		.A( A[27:24] ),
-		.B( B_INT[27:24] ),
-		.CIN( COUT5 ),
-		.COUT( COUT6 ),
-		.SUM( RESULT[27:24] )
-	);
-	
-	cla cla7 (
-		.A( A[31:28] ),
-		.B( B_INT[31:28] ),
-		.CIN( COUT6 ),
-		.COUT( COUT7 ),
-		.SUM( RESULT[31:28] )
-	);
+	// Multiplexer
+	always@(*) begin
+		case(CTRL)
+			3'b000:
+				begin 
+					CIN <= 1'b0;
+					RESULT <= RESULT_SUMSUB;
+				end
+				
+			3'b001:
+				begin
+					CIN <= 1'b1;
+					RESULT <= RESULT_SUMSUB;
+				end
+				
+			3'b010:
+				begin
+					CIN <= 1'b0;
+					RESULT <= RESULT_AND;
+				end
+				
+			3'b011:
+				begin
+					CIN <= 1'b0;
+					RESULT <= RESULT_OR;
+				end
+				
+			3'b100:
+				begin
+					CIN <= 1'b0;
+					RESULT <= A;
+				end
+				
+			default:
+				begin
+					CIN <= 1'b0;
+					RESULT <= A;
+				end
+		endcase
+	end
+		
+	// zeroflag and negativeflag
+	assign ZEROFLAG     = &RESULT[15:0];
+	assign NEGATIVEFLAG = RESULT[15];
 	
 endmodule 
